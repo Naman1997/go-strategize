461119c65e962533e100dfcd6463c058e1149231
Naman Arora <aroranaman17@gmail.com>
2021-12-19 21:21:22 +0530

    Simplify ssh connection using the ssh command

--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/relex/aini v1.5.0
-	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- services/ssh.go
+++ services/ssh.go
@@ -1,19 +1,9 @@
 package services
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
-	"strings"
-
-	"golang.org/x/crypto/ssh"
-	kh "golang.org/x/crypto/ssh/knownhosts"
 )
-
-type connection struct {
-	*ssh.Client
-}
 
 /*
 ValidateConn validates all SSH connections. Does the following:
@@ -22,82 +12,8 @@ ValidateConn validates all SSH connections. Does the following:
 */
 func ValidateConn(username string, privateKey string, homedir string, addr string, port string, strict bool) {
 	privateKey = HomeFix(privateKey, homedir)
-	conn, err := connectSecure(username, privateKey, homedir, addr, port, strict)
-	if err != nil {
-		ColorPrint(ERROR, "[SSH Failure] %v", err)
-	}
-	output, err := conn.sendCommands("echo 'Connected to' `hostname`")
-	if err != nil {
-		ColorPrint(ERROR, "[SSH Failure] [%s] %v", addr, err)
-	}
-	defer conn.Close()
-	ColorPrint(INFO, strings.TrimSuffix(string(output), "\n"))
-}
-
-func connectSecure(username string, privateKey string, homedir string, addr string, port string, strict bool) (*connection, error) {
-	key, err := ioutil.ReadFile(privateKey)
-	if err != nil {
-		ColorPrint(ERROR, "%v", err)
-	}
-
-	// Create the Signer for this private key.
-	signer, err := ssh.ParsePrivateKey(key)
-	if err != nil {
-		ColorPrint(ERROR, "%v", err)
-	}
-
-	//Add fingerprint to known_hosts and copy SSH key
 	copySSHKey(username, addr, port, privateKey+".pub", strict)
-
-	path := "~/.ssh/known_hosts"
-	path = HomeFix(path, homedir)
-	hostKeyCallback, err := kh.New(path)
-	if err != nil {
-		ColorPrint(ERROR, "%v", err)
-	}
-
-	sshConfig := &ssh.ClientConfig{
-		User: username,
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-		},
-		HostKeyCallback: hostKeyCallback,
-	}
-
-	conn, err := ssh.Dial("tcp", addr+":"+port, sshConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return &connection{conn}, nil
-
-}
-
-func (conn *connection) sendCommands(cmds ...string) ([]byte, error) {
-	session, err := conn.NewSession()
-	if err != nil {
-		ColorPrint(ERROR, "%v", err)
-	}
-	defer session.Close()
-
-	modes := ssh.TerminalModes{
-		ssh.ECHO:          0,     // disable echoing
-		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
-		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
-	}
-
-	err = session.RequestPty("xterm", 80, 40, modes)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	cmd := strings.Join(cmds, "; ")
-	output, err := session.Output(cmd)
-	if err != nil {
-		return output, fmt.Errorf("[ERROR] Failed to execute command '%s' on server: %v", cmd, err)
-	}
-
-	return output, err
+	sendCommands(username, addr, port)
 }
 
 func copySSHKey(user string, addr string, port string, key string, strict bool) {
@@ -128,6 +44,26 @@ func copySSHKey(user string, addr string, port string, key string, strict bool) 
 		if err != nil {
 			ColorPrint(ERROR, "[ssh-copy-id] %v", err)
 		}
+	}
+
+	ColorPrint(INFO, "[INFO] Finished executing ssh-copy-id "+addr)
+}
+
+func sendCommands(user string, addr string, port string) {
+	cmd0 := "ssh"
+	cmd1 := user + "@" + addr
+	cmd2 := "-p"
+	cmd3 := port
+	cmd4 := "echo"
+	cmd5 := "'Connected to' $(hostname)"
+
+	cmd := exec.Command(cmd0, cmd1, cmd2, cmd3, cmd4, cmd5)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		ColorPrint(ERROR, "[SSH Failure] %v", err)
 	}
 
 	ColorPrint(INFO, "[INFO] Finished executing ssh-copy-id "+addr)
